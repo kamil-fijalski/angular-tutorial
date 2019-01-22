a6c26ce98eafe6aaf9fbf5ae0a3b4a128e2539a5
kamil-fijalski <kamil.fijalski@outlook.com>
2019-01-22 13:36:27 +0100

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'deborah-shop';
+  pageTitle: string = 'ACME Product Managment';
 }
